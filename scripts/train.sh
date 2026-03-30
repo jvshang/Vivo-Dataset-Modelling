082e2c19ee8f3f8fd18ede1050cd35bc71f826bf
Fahd Abdelazim <54505024+fahdabdelazim@users.noreply.github.com>
2026-03-30 13:18:42 +0000

    add shell script for training using config file

---
+++ scripts/train.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=task1
+#SBATCH --output=task1.out
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+#SBATCH --time=5:00:00
+
+source $HOME/miniforge3/etc/profile.d/conda.sh
+conda activate vivo_dataset
+
+# Print the job has started
+echo "Job Started"
+
+srun python src/train.py --config configs/task1.yaml
